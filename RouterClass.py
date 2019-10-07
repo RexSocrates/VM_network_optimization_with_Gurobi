68ca6805e2efb5fe1d51df9c15ae8d345f714c92
user <user@mail.com>
2019-10-07 17:56:33 +0800

    add edges parameter to the initialization function

--- RouterClass.py
+++ RouterClass.py
@@ -1,10 +1,11 @@
 # Router class
 
 class RouterClass :
-    def __init__(self, routerIndex, contractLength, paymentOption, reservationFee, utilizationFee, onDemandFee) :
+    def __init__(self, routerIndex, contractLength, paymentOption, reservationFee, utilizationFee, onDemandFee, edges) :
         self.routerIndex = routerIndex
         self.contractLength = contractLength
         self.paymentOption = paymentOption
         self.reservationFee = reservationFee
         self.utilizationFee = utilizationFee
         self.onDemandFee = onDemandFee
+        self.edges = edges
